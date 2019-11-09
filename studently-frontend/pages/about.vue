9c98680e8cadcc5563850a4338dcf096f4629455
Daniele Palumbo <daniele@pop-os.localdomain>
2019-11-09 17:04:38 +0100

    Add sidebar component, Add navbar component, Add footer component, Improve Default.vue layout

---
+++ studently-frontend/pages/about.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="content">
+    <h1>About</h1>
+    <p>Some pretty text</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    head() {
+      return {
+        title: "About page",
+        meta: [
+          {
+            hid: "description",
+            name: "description",
+            content: "Studently about page"
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style>
+ .content {
+   min-height: 80vh;
+ }
+</style>
