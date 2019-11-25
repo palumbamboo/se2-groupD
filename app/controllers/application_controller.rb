e294cbb53c4f830ead57d6f87a94fac600b74aeb
Daniele Palumbo <daniele@pop-os.localdomain>
2019-11-25 09:56:01 +0100

    Add super to routing after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     return root_path if resource.roles.size > 1
     return teacher_path(resource.teacher.id) if resource.teacher?
-    parent_path(resource.parent.id) if resource.parent?
+    return parent_path(resource.parent.id) if resource.parent?
+    super
   end
 
 end
