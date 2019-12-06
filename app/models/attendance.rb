a82d331ecfd0c3a0960bec2a7f7b823cad33789f
Giulia Milan <giuliapuntoit96@gmail.com>
2019-12-06 01:02:08 +0100

    Modified type field of attendance table

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,9 +1,12 @@
 class Attendance < ApplicationRecord
   attribute :date, type: Date
-  attribute :type
+  attribute :absence_type
   attribute :enters_at, type: DateTime
   attribute :exits_at, type: DateTime
 
   belongs_to :student
   belongs_to :school_class
+
+  # absence_type should be one of {"Absent", "Late", "Earl"} -> TODO: force input data to be like this
+  # TODO: check constraint that if type=absent, enters_at and exits_at fields are null
 end
--- db/migrate/20191205221220_create_attendances.rb
+++ db/migrate/20191205221220_create_attendances.rb
@@ -2,12 +2,12 @@ class CreateAttendances < ActiveRecord::Migration[6.0]
   def change
     create_table :attendances do |t|
       t.date :date
-      t.string :type
+      t.string :absence_type
       t.datetime :enters_at
       t.datetime :exits_at
 
       t.timestamps
-      
+
       t.belongs_to :student
       t.belongs_to :school_class
     end
