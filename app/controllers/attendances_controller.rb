b666b83cbdcb3c5cb399e63f14c9f4ea85b99ca3
Daniele Palumbo <daniele.palumbo@wanderio.com>
2019-12-10 23:12:56 +0100

    Add assignment management (#63)
    * Add assignment management
    
    * Refactor undo button

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -62,13 +62,13 @@ class AttendancesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_attendance
-      @attendance = Attendance.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_attendance
+    @attendance = Attendance.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def attendance_params
-      params.require(:attendance).permit(:date, :type, :enters_at, :exits_at)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def attendance_params
+    params.require(:attendance).permit(:date, :type, :enters_at, :exits_at)
+  end
 end
