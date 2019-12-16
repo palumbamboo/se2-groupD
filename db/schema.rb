c3d86b0ae48c871bdb06842c90ecfcb822c7f473
Daniele Palumbo <daniele@pop-os.localdomain>
2019-12-16 23:05:35 +0100

    Align schema with develop

--- db/schema.rb
+++ db/schema.rb
@@ -15,13 +15,6 @@ ActiveRecord::Schema.define(version: 2019_11_26_151201) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "administrators", force: :cascade do |t|
-    t.string "name"
-    t.string "surname"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "assignments", force: :cascade do |t|
     t.string "name"
     t.datetime "expiry_date"
