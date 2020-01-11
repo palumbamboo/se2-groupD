c0c53855ed91c24eaa4c96d508d4ad56d63b62dc
Giulia Milan <giuliapuntoit96@gmail.com>
2020-01-11 23:47:05 +0100

    Bug fixing

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def print_pretty_errors
+    errors.messages.values.flatten.to_sentence
+  end
+
 end
--- app/models/mark.rb
+++ app/models/mark.rb
@@ -10,6 +10,10 @@ class Mark < ApplicationRecord
   validates :mark, presence: true, inclusion: { in: 0..10, message: 'Mark value must be between 0 and 10' }
   validates :subject, presence: true, inclusion: { in: SUBJECTS, message: 'Invalid subject' }
   validates :date, presence: true, inclusion: { in: START_OF_THE_YEAR..END_OF_THE_YEAR, message: "Mark date must be in the current scholastic year #{ACADEMIC_YEAR}" }
-  validates :notes, presence: true, format: { with: /\A[\w[:punct:]\s]*\z/, message: 'No special characters, only letters and numbers' }
+  validates :notes, format: { with: /\A[\w[:punct:]\s]*\z/, message: 'No special characters, only letters and numbers' }
   validates :student_id, :teacher_id, presence: true
+
+  def mark= value
+    super(value.to_f)
+  end
 end
